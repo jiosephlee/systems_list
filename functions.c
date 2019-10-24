dd2407069b4238cfe0fade8ae48c9cc32b57f334
Joseph Lee <jlee01@stuy.edu>
2019-10-24 08:04:08 -0400

    editing devlog

---
+++ functions.c
@@ -0,0 +1,8 @@
+void print_list(struct node *){
+
+}
+struct node * insert_front(struct node *, int input){
+  struct node * output = malloc(8);
+  output
+}
+struct node * free_list(struct node *);
---
+++ main.c
@@ -0,0 +1,13 @@
+struct node {
+  int i;
+  struct node *next;
+};
+
+#include <stdio.h>
+#include <time.h>
+#include "headers.h"
+
+int main() {
+  /* code */
+  return 0;
+}
